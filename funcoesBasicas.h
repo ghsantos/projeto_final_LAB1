b054c7b3366f208fc3a68c958aa57726d331d043
isnardsilva <isnardsilva28@gmail.com>
2017-05-22 20:28:41 -0300

    modificando a funcao que pede que o usuario digite Enter

--- funcoesBasicas.h
+++ funcoesBasicas.h
@@ -8,13 +8,14 @@ float leValidaFloat(char *msg, char *msgErro, float valMin, float valMax);
 void leValidaTexto(char *msg, char *msgErro, char *texto, int tamMinTexto, int tamMaxTexto);
 void tiraTerminador(char *texto);
 char leValidaOpcao(char *msg, char *msgErro, char *opcoesValidas);
+int verifCPFValido(char *cpf);
 void toLowerStr(char *str);
 void toUpperStr(char *str);
 int geraNroIntAleatorio(int valMin, int valMax);
 Data obtemDataSistema(void);
 Horario obtemHoraSistema(void);
 int verificaArqExiste(char *nomeArq);
-void continuarComEnter(void);
+void continuarComEnter(char *msg);
 void apresentaOpcoesMenu(void);
 
 #endif // FUNCOES_BASICAS_H
