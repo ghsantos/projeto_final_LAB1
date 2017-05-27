a9968c32b5d2e2d006ac3ff9eafec18bc93b3a76
isnardsilva <isnardsilva28@gmail.com>
2017-05-27 19:31:06 -0300

    otimizando os includes

--- academia.h
+++ academia.h
@@ -1,6 +1,10 @@
 #ifndef ACADEMIA_H
 #define ACADEMIA_H
 
+#include "definicoes.h"
+#include "estruturas.h"
+#include "funcoesBasicas.h"
+
 int verifArqAcademiaValido(void);
 void apresentaDadosAcademia(void);
 int cadastraAcademia(void);
