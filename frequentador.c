6a46974f6077ee656654c321dc5cbf6038ff9eb7
isnardsilva <isnardsilva28@gmail.com>
2017-05-27 19:31:41 -0300

    refatorando o código

--- frequentador.c
+++ frequentador.c
@@ -1,56 +1,24 @@
-#include "definicoes.h"
-#include "estruturas.h"
-#include "funcoesBasicas.h"
 #include "frequentador.h"
 
 /*
-	Objetivo: Cadastrar um frequentador gravando os seus dados em um arquivo de saida
+	Objetivo: Obter a qtd de frequentadores cadastrados
 	Parametros: nenhum
-	Retorno: nenhum
+	Retorno: qtd de cadastros
 */
-void cadastraFrequentador(void){
+int obtemQtdFreqCadastrados(void){
+	FILE *arq;
 	Frequentador frequentador;
-	int cpfValido;
+	int qtdFrequentadores=0;
 	
-	
-	printf("CADASTRAR FREQUENTADOR\n\n");
-	
-	// Gerando uma matricula aleatorio (unica)
-	do{
-		frequentador.matricula = geraNroIntAleatorio(VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
-	}while(obtemPosicaoFreqArq(frequentador.matricula) != 0);
-	printf("Matricula gerada: %d\n", frequentador.matricula);
-	
-	// Obtendo o nome
-	leValidaTexto("\nNome completo: ", "Nome invalido... Digite no minimo 3 caracteres: ", frequentador.nome, TAM_MIN_NOME_FREQ, TAM_MAX_NOME_FREQ);
-	
-	// Lendo e validando o CPF
-	do{
-		leValidaTexto("\nCPF (apenas nros): ", "CPF invalido... Digite os 11 digitos: ", frequentador.cpf, (TAM_CPF-1), TAM_CPF);
-		cpfValido = verifCPFValido(frequentador.cpf);
-		
-		if(cpfValido == 0){
-			printf("CPF invalido! Digite apenas os nros.\n");
+	arq = fopen(NOME_ARQ_FREQ, "rb");
+	if(arq != NULL){
+		if(fseek(arq, 0, SEEK_END) == 0){
+			qtdFrequentadores = ftell(arq) / sizeof(Frequentador);
 		}
-	}while(cpfValido == 0);
-	
-	// Obtendo o peso
-	frequentador.peso = leValidaFloat("\nPeso (Kg): ", "Peso invalido... Digite novamente: ", VAL_MIN_PESO, VAL_MAX_PESO);
-	
-	// Obtendo o sexo
-	frequentador.sexo = leValidaOpcao("\nSexo[M/f]: ", "Sexo invalido... Digite apenas M ou F: ", "MF");
-	
-	// Obtendo a data de ingresso na academia (data atual do sistema)
-	frequentador.dataIngresso = obtemDataSistema();
-	
-	// Gravando os dados coletados em um arquivo de saida
-	if(gravaDadosFreqArq(&frequentador) == 0){
-		printf("\n\nErro ao tentar cadastrar o frequentador!");
-	} else {
-		printf("\n\nFrequentador cadastrado com sucesso!");
+		fclose(arq);
 	}
 	
-	continuarComEnter("\n\nPressione [Enter] para continuar...");
+	return qtdFrequentadores;
 }
 
 /*
@@ -83,23 +51,27 @@ int obtemPosicaoFreqArq(int matriculaPesq){
 }
 
 /*
-	Objetivo: Gravar os dados de um frequentador em um arquivo
-	Parametros: endereco de memoria da struct com os dados do frequentador
-	Retorno: 0(erro no cadastro) ou 1(cadastro concluido)
+	Objetivo: Obter os dados de um frequentador a partir da sua posicao dentro de um arquivo de entrada
+	Parametros: endereco de memoria da struct(frequentador) que vai armazenar os dados, posicao dos dados
+		do frequentador no arquivo
+	Retorno: 0(os dados nao foram recuperador) ou 1(os dados foram recuperados com exito)
 */
-int gravaDadosFreqArq(Frequentador *frequentador){
+int obtemDadosFrequentador(Frequentador *frequentador, int posicaoFreqArq){
 	FILE *arq;
-	int gravacaoConcluida=0;
+	int freqEncontrado=0;
 	
-	arq = fopen(NOME_ARQ_FREQ, "ab");
+	arq = fopen(NOME_ARQ_FREQ, "rb");
 	if(arq != NULL){
-		if(fwrite(frequentador, sizeof(Frequentador), 1, arq) == 1){
-			gravacaoConcluida=1;
+		if(fseek(arq, sizeof(Frequentador)*(posicaoFreqArq-1), SEEK_SET) == 0){
+			if(fread(frequentador, sizeof(Frequentador), 1, arq) == 1){
+				freqEncontrado=1;
+			}
 		}
+		
 		fclose(arq);
 	}
 	
-	return gravacaoConcluida;
+	return freqEncontrado;
 }
 
 /*
@@ -131,81 +103,194 @@ void apresentaDadosFrequentadores(void){
 }
 
 /*
+	Objetivo: Apresentar os dados de um frequentador
+	Parametros: endereco de memoria da struct com os dados a serem exibidos
+	Retorno: nenhum
+*/
+void apresentaDadosFrequentador(Frequentador *frequentador){
+	printf("\nMatricula: %d\n", frequentador->matricula);
+	printf("Nome: %s\n", frequentador->nome);
+	printf("CPF: %s\n", frequentador->cpf);
+	printf("Peso: %.2f Kg\n", frequentador->peso);
+	printf("Sexo: %s\n", frequentador->sexo == 'M' ? "Masculino" : "Feminino");
+	printf("Data de ingresso na academia: %02d/%02d/%d\n", frequentador->dataIngresso.dia, frequentador->dataIngresso.mes, frequentador->dataIngresso.ano);
+}
+
+/*
+	Objetivo: Cadastrar um frequentador gravando os seus dados em um arquivo de saida
+	Parametros: nenhum
+	Retorno: nenhum
+*/
+void cadastraFrequentador(void){
+	Frequentador frequentador;
+	int cpfValido;
+	
+	apresentaDadosAcademia();
+	printf("\nCADASTRAR FREQUENTADOR\n\n");
+	
+	// Gerando uma matricula aleatorio (unica)
+	do{
+		frequentador.matricula = geraNroIntAleatorio(VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
+	}while(obtemPosicaoFreqArq(frequentador.matricula) != 0);
+	printf("Matricula gerada: %d\n", frequentador.matricula);
+	
+	// Obtendo o nome
+	leValidaTexto("\nNome completo: ", "Nome invalido... Digite novamente: ", frequentador.nome, TAM_MIN_NOME_FREQ, TAM_MAX_NOME_FREQ);
+	
+	// Lendo e validando o CPF
+	do{
+		leValidaTexto("\nCPF (apenas nros): ", "CPF invalido... Digite os 11 digitos: ", frequentador.cpf, (TAM_CPF-1), TAM_CPF);
+		cpfValido = verifCPFValido(frequentador.cpf);
+		
+		if(cpfValido == 0){
+			printf("CPF invalido! Digite apenas os nros.\n");
+		}
+	}while(cpfValido == 0);
+	
+	// Obtendo o peso
+	frequentador.peso = leValidaFloat("\nPeso (Kg): ", "Peso invalido... Digite novamente: ", VAL_MIN_PESO, VAL_MAX_PESO);
+	
+	// Obtendo o sexo
+	frequentador.sexo = leValidaOpcao("\nSexo[M/f]: ", "Sexo invalido... Digite apenas M ou F: ", "MF");
+	
+	// Obtendo a data de ingresso na academia (data atual do sistema)
+	frequentador.dataIngresso = obtemDataSistema();
+	
+	// Gravando os dados coletados em um arquivo de saida
+	if(gravaDadosFreqNovo(&frequentador) == 0){
+		printf("\n\nErro ao tentar cadastrar o frequentador!");
+	} else {
+		printf("\n\nFrequentador cadastrado com sucesso!");
+	}
+	
+	continuarComEnter("\n\nPressione [Enter] para continuar...");
+}
+
+/*
+	Objetivo: Gravar os dados de um novo frequentador em um arquivo
+	Parametros: endereco de memoria da struct com os dados do frequentador
+	Retorno: 0(erro no cadastro) ou 1(cadastro concluido)
+*/
+int gravaDadosFreqNovo(Frequentador *frequentador){
+	FILE *arq;
+	int gravacaoConcluida=0;
+	
+	arq = fopen(NOME_ARQ_FREQ, "ab");
+	if(arq != NULL){
+		if(fwrite(frequentador, sizeof(Frequentador), 1, arq) == 1){
+			gravacaoConcluida=1;
+		}
+		fclose(arq);
+	}
+	
+	return gravacaoConcluida;
+}
+
+/*
 	Objetivo: Alterar os dados de um frequentador (com excecao da matricula)
 	Parametros: nenhum
 	Retorno: nenhum
 */
 void alteraDadosFrequentador(void){
 	Frequentador frequentador;
-	int matriculaPesq, posicaoFreqArq, cpfValido;
-	char opcaoAlteracao;
+	int matriculaPesq, posicaoFreqArq;
 	
-	printf("ALTERAR DADOS DO FREQUENTADOR\n\n");
-	apresentaDadosFrequentadores();
+	apresentaDadosAcademia();
 	
-	// Obtendo a matricula do funcionario a ser alterado
-	matriculaPesq = leValidaInt("\n\nMatricula do frequentador a ser alterado: ", "Matricula invalida... Digite novamente: ", VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
-	// Obtendo (caso exista) a posicao dos dados do frequentador da matricula coletada dentro do arquivo de entrada
-	posicaoFreqArq = obtemPosicaoFreqArq(matriculaPesq);
-	
-	// Verificando se foi encontrada a posicao com os dados
-	if(posicaoFreqArq == 0){
-		printf("\n\nNenhum frequentador com essa matricula foi encontrado!\n");
-	}  else {
-		// Obtendo todos os dados do frequentador 'marcado' para alteracao
-		if(obtemDadosFrequentadorArq(&frequentador, posicaoFreqArq) == 0){
-			printf("\n\nErro ao tentar recuperar os dados desse frequentador no arquivo!\n");
+	// Verificando se tem frequentadores cadastrados
+	if(obtemQtdFreqCadastrados() == 0){
+		printf("\n\nNao existem frequentadores cadastrados!\n");
+	} else {
+		printf("\n\n");
+		apresentaDadosFrequentadores();
+		matriculaPesq = leValidaInt("\n\nMatricula do frequentador a ser alterado: ", "Matricula invalida... Digite novamente: ", VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
+		
+		// Verificando se a matricula existe
+		posicaoFreqArq = obtemPosicaoFreqArq(matriculaPesq);
+		if(posicaoFreqArq == 0){
+			printf("\n\nNao foi encontrado nenhum frequentador com essa matricula!");
 		} else {
-			apresentaDadosFrequentador(&frequentador);
-			opcaoAlteracao = leValidaOpcao("\nDeseja alterar esses dados[S/n]: ", "Opcao invalida... Digite apenas S ou N: ", "SN");
-			
-			// Verificando se o usuario deseja realmente alterar o tal frequentador
-			if(opcaoAlteracao == 'N'){
-				printf("\n\nNenhum dado foi modificado!\n");
+			// Obtendo os dados do frequentador selecionado
+			if(obtemDadosFrequentador(&frequentador, posicaoFreqArq) == 0){
+				printf("\n\nErro ao tentar recuperar os dados do frequentador selecionado!");
 			} else {
-				opcaoAlteracao = leValidaOpcao("\nAlterar = \n[N]-Nome ou [C]-CPF ou [P]-Peso ou [S]-Sexo ou [D]-Data de ingresso: ", "Opcao invalida... Digite novamente: ", "NCPSD");
-				switch(opcaoAlteracao){
-					case 'N': // Altera nome
-						leValidaTexto("\nNovo Nome: ", "Nome invalido... Digite no minimo 3 caracteres: ", frequentador.nome, TAM_MIN_NOME_FREQ, TAM_MAX_NOME_FREQ);
-						break;
-					
-					case 'C':
-						do{
-							leValidaTexto("\nNovo CPF (apenas nros): ", "CPF invalido... Digite os 11 digitos: ", frequentador.cpf, (TAM_CPF-1), TAM_CPF);
-							cpfValido = verifCPFValido(frequentador.cpf);
-							
-							if(cpfValido == 0){
-								printf("CPF invalido! Digite apenas os nros.\n");
-							}
-						}while(cpfValido == 0);
-						break;
-						
-					case 'P':
-						frequentador.peso = leValidaFloat("\nNovo Peso (Kg): ", "Peso invalido... Digite novamente: ", VAL_MIN_PESO, VAL_MAX_PESO);
-						break;
-						
-					case 'S':
-						frequentador.sexo = leValidaOpcao("\nNovo Sexo[M/f]: ", "Sexo invalido... Digite apenas M ou F: ", "MF");
-						break;
-						
-					case 'D':
-						frequentador.dataIngresso = obtemDataSistema();
-						printf("\nUma nova data de ingresso foi obtida!");
-						break;
-				}
-				
-				// Tentando gravar os novos dados do frequentador no arquivo de saida
-				if(gravaDadosFreqAlterados(&frequentador, posicaoFreqArq) == 0){
-					printf("\n\nErro ao tentar alterar os dados!\n");
+				// Verificando se o usuario modificou algum dado do frequentador
+				if(modificaFrequentador(&frequentador) == 0){
+					printf("\n\nNenhum dado foi modificado!");
 				} else {
-					printf("\n\nDados alterados com sucesso!\n");
+					// Tentando gravar os novos dados do frequentador
+					if(gravaDadosFreqAlterado(&frequentador, posicaoFreqArq) == 0){
+						printf("\n\nErro ao tentar alterar os dados");
+					} else {
+						printf("\n\nDados alterados com sucesso!");
+					}
 				}
 			}
 		}
 	}
 	
-	
 	continuarComEnter("\n\nPressione [Enter] para continuar...");
+}
+
+/*
+	Objetivo: Modificar os dados de um frequentador
+	Parametros: endereco de memoria da struct com os dados do frequentador
+	Retorno: 0(nao houve alteracao nos dados) ou 1(houve alteracao nos dados)
+*/
+int modificaFrequentador(Frequentador *frequentador){
+	char opcaoDesejada;
+	int dadosModificados=0;
+	
+	LIMPA_TELA;
+	apresentaDadosAcademia();
+	apresentaDadosFrequentador(frequentador);
+	opcaoDesejada = leValidaOpcao("\n\nDeseja alterar os dados desse frequentador[S/n]: ", "Opcao invalida... Digite apenas S ou N: ", "SN");
+	
+	if(opcaoDesejada == 'S'){
+		dadosModificados=1;
+		printf("\n\nAlteracoes Possiveis: \n");
+		printf("N - Nome\n");
+		printf("C - CPF\n");
+		printf("P - Peso\n");
+		printf("S - Sexo\n");
+		printf("D - Data de ingresso\n");
+		printf("Z - Cancelar alteracao\n\n");
+		opcaoDesejada = leValidaOpcao("Opcao desejada: ", "Opcao invalida... Digite novamente: ", "NCPSDZ");
+		
+		switch(opcaoDesejada){
+			case 'N':{
+				leValidaTexto("\nNovo nome: ", "Nome invalido... Digite ao menos 3 caracteres: ", frequentador->nome, TAM_MIN_NOME_FREQ, TAM_MAX_NOME_FREQ);
+				break;
+			}
+				
+			case 'C':{
+				
+				break;
+			}
+			
+			case 'P':{
+				frequentador->peso = leValidaFloat("\nNovo peso: ", "Peso invalido... Digite novamente: ", VAL_MIN_PESO, VAL_MAX_PESO);
+				break;
+			}
+			
+			case 'S':{
+				frequentador->sexo = leValidaOpcao("\nNovo sexo[M/f]: ", "Opcao invalida... Digite apenas M ou F: ", "MF");
+				break;
+			}
+			
+			case 'D':{
+				
+				break;
+			}
+			
+			case 'Z':{
+				dadosModificados=0;
+				break;
+			}
+		}
+	}
+	
+	return dadosModificados;
 }
 
 /*
@@ -213,7 +298,7 @@ void alteraDadosFrequentador(void){
 	Parametros: endereco de memoria da struct com os dados, posicao do frequentador no arquivo
 	Retorno: 0(erro na gravacao) ou 1(gravacao concluida)
 */
-int gravaDadosFreqAlterados(Frequentador *frequentador, int posicaoFreqArq){
+int gravaDadosFreqAlterado(Frequentador *frequentador, int posicaoFreqArq){
 	FILE *arq;
 	int gravacaoConcluida=0;
 	
@@ -232,44 +317,6 @@ int gravaDadosFreqAlterados(Frequentador *frequentador, int posicaoFreqArq){
 }
 
 /*
-	Objetivo: Obter os dados de um frequentador a partir da sua posicao dentro de um arquivo de entrada
-	Parametros: endereco de memoria da struct(frequentador) que vai armazenar os dados, posicao dos dados
-		do frequentador no arquivo
-	Retorno: 0(os dados nao foram recuperador) ou 1(os dados foram recuperados com exito)
-*/
-int obtemDadosFrequentadorArq(Frequentador *frequentador, int posicaoFreqArq){
-	FILE *arq;
-	int freqEncontrado=0;
-	
-	arq = fopen(NOME_ARQ_FREQ, "rb");
-	if(arq != NULL){
-		if(fseek(arq, sizeof(Frequentador)*(posicaoFreqArq-1), SEEK_SET) == 0){
-			if(fread(frequentador, sizeof(Frequentador), 1, arq) == 1){
-				freqEncontrado=1;
-			}
-		}
-		
-		fclose(arq);
-	}
-	
-	return freqEncontrado;
-}
-
-/*
-	Objetivo: Apresentar os dados de um frequentador
-	Parametros: endereco de memoria da struct com os dados a serem exibidos
-	Retorno: nenhum
-*/
-void apresentaDadosFrequentador(Frequentador *frequentador){
-	printf("\nMatricula: %d\n", frequentador->matricula);
-	printf("Nome: %s\n", frequentador->nome);
-	printf("CPF: %s\n", frequentador->cpf);
-	printf("Peso: %.2f Kg\n", frequentador->peso);
-	printf("Sexo: %s\n", frequentador->sexo == 'M' ? "Masculino" : "Feminino");
-	printf("Data de ingresso na academia: %02d/%02d/%d\n", frequentador->dataIngresso.dia, frequentador->dataIngresso.mes, frequentador->dataIngresso.ano);
-}
-
-/*
 	Objetivo: Excluir um frequentador
 	Parametros: nenhum
 	Retorno: nenhum
@@ -277,42 +324,50 @@ void apresentaDadosFrequentador(Frequentador *frequentador){
 void excluiFrequentador(void){
 	Frequentador frequentador;
 	int matriculaPesq, posicaoFreqArq;
-	char desejaExcluir;
+	char opcaoDesejada;
 	
-	printf("EXCLUIR FREQUENTADOR\n\n");
-	apresentaDadosFrequentadores();
 	
-	// Coletando a matricula do frequentador a ser excluido
-	matriculaPesq = leValidaInt("\n\nMatricula do frequentador a ser excluido: ", "Matricula invalida... Digite novamente: ", VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
-	// Verificando se o frequentador com tal matricula existe no arquivo de entrada
-	posicaoFreqArq = obtemPosicaoFreqArq(matriculaPesq);
+	apresentaDadosAcademia();
 	
-	if(posicaoFreqArq == 0){
-		printf("\n\nNenhum frequentador com essa matricula foi encontrado!\n");
-	}  else {
-		// Obtendo todos os dados do frequentador em questao ('marcado')
-		if(obtemDadosFrequentadorArq(&frequentador, posicaoFreqArq) == 0){
-			printf("\n\nErro ao tentar recuperar os dados desse frequentador no arquivo!\n");
+	if(obtemQtdFreqCadastrados() == 0){
+		printf("\n\nNao existem frequentadores cadastrados!\n");
+	} else {
+		printf("\n\n");
+		apresentaDadosFrequentadores();
+		matriculaPesq = leValidaInt("\n\nMatricula do frequentador a ser excluido: ", "Matricula invalida... Digite novamente: ", VAL_MIN_MATRIC_FREQ, VAL_MAX_MATRIC_FREQ);
+		
+		// Verificando se a matricula existe
+		posicaoFreqArq = obtemPosicaoFreqArq(matriculaPesq);
+		if(posicaoFreqArq == 0){
+			printf("\n\nNao foi encontrado nenhum frequentador com essa matricula!");
 		} else {
-			apresentaDadosFrequentador(&frequentador);
-			desejaExcluir = leValidaOpcao("\nDeseja excluir esse frequentador[S/n]: ", "Opcao invalida... Digite apenas S ou N: ", "SN");
-			
-			// Verificando se o usuario quer realmente excluir o frequentador marcado
-			if(desejaExcluir == 'N'){
-				printf("\n\nNenhum frequentador foi excluido!\n");
+			// Obtendo os dados do frequentador selecionado
+			if(obtemDadosFrequentador(&frequentador, posicaoFreqArq) == 0){
+				printf("\n\nErro ao tentar recuperar os dados do frequentador!");
 			} else {
-				// Verificando se o frequentador ja executou ao menos uma atividade na academia
-				if(verifFreqDevAtividades(frequentador.matricula) == 1){
-					printf("\n\nErro ao tentar excluir o frequentador!\nEste frequentador ja desenvolveu alguma serie de exercicios!\n");
+				LIMPA_TELA;
+				apresentaDadosAcademia();
+				apresentaDadosFrequentador(&frequentador);
+				opcaoDesejada = leValidaOpcao("\n\nDeseja excluir esse frequentador[S/n]: ", "Opcao invalida... Digite apenas S ou N: ", "SN");
+				
+				if(opcaoDesejada == 'N'){
+					printf("\n\nNenhum frequentador foi excluido!");
 				} else {
-					// Removendo os dados do frequentador
-					if(removeFreqArq(frequentador.matricula) == 0){
-						printf("\n\nErro ao tentar excluir o frequentador!\n");
+					// Verificando se o frequentador já executou alguma atividade na academia
+					if(verifFreqDevAtividades(frequentador.matricula) == 1){
+						printf("\n\nEsse frequentador nao pode ser excluido!\n");
+						printf("Motivo: esse frequentador possui atividades cadastradas!");
 					} else {
-						printf("\n\nFrequentador excluido com sucesso!\n");
+						// Tentando excluir o frequentador
+						if(removeDadosFreqArq(frequentador.matricula) == 0){
+							printf("\n\nErro ao tentar excluir o frequentador!");
+						} else {
+							printf("\n\nFrequentador excluido com sucesso!");
+						}
 					}
 				}
 			}
+			
 		}
 	}
 	
@@ -350,7 +405,7 @@ int verifFreqDevAtividades(int matriculaVerif){
 	Parametros: matricula do frequentador que sera excluido
 	Retorno: 0(erro na exclusao) ou 1(exclusao concluida)
 */
-int removeFreqArq(int matriculaFreqExc){
+int removeDadosFreqArq(int matriculaFreqExc){
 	Frequentador frequentador;
 	FILE *arq, *arqAux;
 	int remocaoConcluida=1;
