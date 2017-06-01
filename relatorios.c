f95a2cb73fb8fff2b36a25c3b3939bb98ffdce0d
isnardsilva <isnardsilva28@gmail.com>
2017-06-01 17:17:57 -0300

    implementando relatórios

--- relatorios.c
+++ relatorios.c
@@ -20,9 +20,11 @@ void geraRelatorios(void){
 				break;
 				
 			case 'B':
+				apresentaTodosCadastros();
 				break;
 				
 			case 'C':
+				pesqFrequentadoresPeloNome();
 				break;
 				
 			case 'D':
@@ -32,4 +34,46 @@ void geraRelatorios(void){
 				break;
 		}
 	}while(opcaoDesejada != 'Z');
+}
+
+/*
+	Objetivo: Controlar a exibicao de todos os frequentadores, series e atividades cadastradas
+		no sistema
+	Parametros: nenhum
+	Retorno: nenhum
+*/
+void apresentaTodosCadastros(void){
+	char opcaoDesejada;
+	
+	apresentaDadosAcademia();
+	printf("\nDeseja ver todos os cadastros de: \n");
+	printf("F - Frequentadores\n");
+	printf("S - Series de exercicios\n");
+	printf("A - Atividades desenvolvidas\n");
+	printf("Z - Retornar a tela anterior\n\n");
+	opcaoDesejada = leValidaOpcao("Opcao desejada: ", "Opcao invalida... Digite novamente: ", "FSAZ");
+	LIMPA_TELA;
+	
+	switch(opcaoDesejada){
+		case 'F':{
+			apresentaDadosAcademia();
+			printf("\n");
+			apresentaDadosFrequentadoresArq();
+			break;
+		}
+		
+		case 'S':{
+			apresentaDadosAcademia();
+			printf("\n");
+			apresentaDadosSeriesExsArq();
+			break;
+		}
+		
+		case 'Z':{
+			return;
+			break;
+		}
+	}
+	
+	continuarComEnter("\n\nPressione [Enter] para continuar...");
 }
