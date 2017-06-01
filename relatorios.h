c1bc347275a02a2572c7423c18be3654876292ae
isnardsilva <isnardsilva28@gmail.com>
2017-06-01 15:18:04 -0300

    arquivo de manipulação de relatórios

---
+++ relatorios.h
@@ -0,0 +1,11 @@
+#ifndef RELATORIOS_H
+#define RELATORIOS_H
+
+#include "definicoes.h"
+#include "funcoesBasicas.h"
+#include "estruturas.h"
+#include "academia.h"
+
+void geraRelatorios(void);
+
+#endif	// RELATORIOS_H
