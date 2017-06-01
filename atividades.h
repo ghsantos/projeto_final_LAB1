4084e4d0ca38275561eebbf95359a61d281f916e
isnardsilva <isnardsilva28@gmail.com>
2017-06-01 20:50:48 -0300

    implementando os relatórios

--- atividades.h
+++ atividades.h
@@ -7,17 +7,25 @@
 #include "frequentador.h"
 #include "series.h"
 
-void apresentaDadosAtivsDesenvolvidas(void);
+void apresentaDadosAtividadesArq(void);
+void apresentaDadosAtividadesArqIndice(void);
 void apresentaDadosAtivDesenvolvida(AtividadeDesenvolvida *ativDesenvolvida);
-int obtemQtdAtivDesevCadastradas(void);
-int fezAtividadeHoje(AtividadeDesenvolvida atividade);
+void apresentaAtivsFrequentador(int matriculaPesq);
+
 int obtemPosicaoAtivDesenvArq(int matricFreqPesq, int idSerieExPesq);
-int obtemDadosAtivDesenvolvida(AtividadeDesenvolvida *ativDesenvolvida, int posicaAtivDesevArq);
+int obtemQtdAtivDesevCadastradas(void);
+int obtemDadosAtivPorChaveUnica(AtividadeDesenvolvida *ativDesenvPesq);
+int obtemDadosAtivPorPosicaoArq(AtividadeDesenvolvida *ativDesenvolvida, int posicaAtivDesevArq);
+
+int verifFreqFezAtividadeHoje(AtividadeDesenvolvida atividade);
+int verifFreqFezAlgumaAtividade(int matriculaVerif);
 
 void cadastraAtivDesenvolvida(void);
 int gravaDadosAtivDesenvolvidaArq(AtividadeDesenvolvida *ativDesenvolvida);
 
 void excluiAtivDesenvolvida(void);
 int removeDadosAtivDesevArq(int indice);
+
+void pesqAtivDesevPorChaveUnica(void);
 
 #endif	// ATIVIDADES
