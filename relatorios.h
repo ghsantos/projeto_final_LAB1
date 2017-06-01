584eac0c38426cb1cd879d9c406e129f59de2539
isnardsilva <isnardsilva28@gmail.com>
2017-06-01 17:18:07 -0300

    implementando relatórios

--- relatorios.h
+++ relatorios.h
@@ -5,7 +5,10 @@
 #include "funcoesBasicas.h"
 #include "estruturas.h"
 #include "academia.h"
+#include "frequentador.h"
+#include "series.h"
 
 void geraRelatorios(void);
+void apresentaTodosCadastros(void);
 
 #endif	// RELATORIOS_H
